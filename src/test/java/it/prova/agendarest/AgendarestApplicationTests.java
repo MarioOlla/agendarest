fc4165b2722756f839556e7bfa19daac47cff90b
MarioOlla <mario.olla25@gmail.com>
2022-11-28 13:48:31 +0100

    primo commit

---
+++ src/test/java/it/prova/agendarest/AgendarestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.agendarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
